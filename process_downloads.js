f7970ea5ca861edf5501bbb084fae1d57062e31e
Jamie Lawrence <jamie@shutterscouts.com>
2011-12-11 01:14:19 +0000

    Process downloads using a temp file to avoid sending chunked encoded files to dropbox

--- process_downloads.js
+++ process_downloads.js
@@ -1,23 +1,35 @@
 var urlpipe = require('./urlpipe_utils');
 var request = require('request');
+var temp = require('temp');
+var fs = require('fs');
 
 function run_task(urlkey, url, filename, oauth_token, oauth_token_secret){
 	var options = {oauth_token: oauth_token, oauth_token_secret: oauth_token_secret}
     // download the file (and follow redirects?) and pipe to dropbox
     //FIXME: If the download is chunked encoding, the upload to dropbox will fail. I need to catch this
     //       and handle it correctly. It currently kills the process :(
-    // Perhaps change to download to a temp file and the upload from there 
-    request({url: url}).pipe(urlpipe.dropbox.put_request(filename, options, function(status, reply){
+    // Perhaps change to download to a temp file and the upload from there
+    temp.open({prefix: 'urlpipe_download_', suffix: '_'+filename}, function(err, temp_file){
+    console.log(temp_file.path);
+    var pipe_to_file = request({url: url}).pipe(fs.WriteStream(temp_file.path));
+    pipe_to_file.end = function() {
+      var dropbox_upload = urlpipe.dropbox.put_request(filename, options, function(status, reply){
         console.log(status);
         console.log(reply);
         var task_status = 'completed';
-        if(reply.statusCode == 200) {
+        if(reply.statusCode != 200) {
           task_status = 'failed';
         }
         urlpipe.redis.hmset(urlkey, ['status', task_status, 'error', reply.statusCode], function(err, value){
           setTimeout(check_queue, 1000);
         });
-	}));
+      });
+      dropbox_upload.headers['Content-Length'] = fs.statSync(temp_file.path).size;
+      console.dir(dropbox_upload);
+
+      fs.ReadStream(temp_file.path).pipe(dropbox_upload);
+    }
+	});
 } 
 
 function fetch_task(){
